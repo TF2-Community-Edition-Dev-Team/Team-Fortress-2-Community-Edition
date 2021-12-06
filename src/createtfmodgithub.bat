b9fce1379e5e2b9ef1ceff06c6173529682c9a08
Yuuki <30376282+Yuuki795@users.noreply.github.com>
2021-12-06 10:32:17 -0500

    Update createtfmodgithub.bat

--- src/createtfmodgithub.bat
+++ src/createtfmodgithub.bat
@@ -1,2 +1,3 @@
 cd src
+reg add HKLM\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\10.0\Projects\{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942} /v DefaultProjectExtension /t REG_SZ /d vcproj
 devtools\bin\vpc.exe /tf_mod +game /mksln games.sln
