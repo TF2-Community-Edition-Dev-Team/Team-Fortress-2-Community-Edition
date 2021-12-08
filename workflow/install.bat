cfb47723638970a3a610ec256a464ef24e698d37
Yuuki <30376282+Yuuki795@users.noreply.github.com>
2021-12-08 08:46:37 -0500

    Update install.bat

--- workflow/install.bat
+++ workflow/install.bat
@@ -1,1 +1,1 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\devenv.exe" ./src/games.sln /upgrade
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\devenv.exe" src\games.sln /upgrade
