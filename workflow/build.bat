26354e099170efeda0ccaec17878f8c7833a7128
Yuuki <30376282+Yuuki795@users.noreply.github.com>
2021-12-08 08:40:44 -0500

    Update build.bat

--- workflow/build.bat
+++ workflow/build.bat
@@ -1,2 +1,2 @@
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe" games.sln /p:Configuration=Release /p:Platform=Win32 /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe" ./src/games.sln /p:Configuration=Release /p:Platform=Win32 /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
