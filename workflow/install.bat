bf1e51c21c32ed82509d3b2a60903c20db1ebd1b
Yuuki <30376282+Yuuki795@users.noreply.github.com>
2021-12-08 08:22:57 -0500

    Update install.bat

--- workflow/install.bat
+++ workflow/install.bat
@@ -1,4 +1,1 @@
-cd C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\
-sleep 5
-devenv.exe D:\a\Team-Fortress-2-Community-Edition\Team-Fortress-2-Community-Edition\games.sln /upgrade
-
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe" games.sln /upgrade
