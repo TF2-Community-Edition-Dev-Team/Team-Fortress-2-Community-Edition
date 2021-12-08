c54c6ce34ef62bcd27fafe4c53dfc6b80597d029
Yuuki <30376282+Yuuki795@users.noreply.github.com>
2021-12-08 08:07:43 -0500

    Create build.bat

---
+++ workflow/build.bat
@@ -0,0 +1,2 @@
+cd ..\src
+C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\MsBuild.exe games.sln /p:Configuration=Release /p:Platform=Win32 /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll
